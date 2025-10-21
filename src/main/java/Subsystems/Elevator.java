5239486238695dc3f88519ad84650c8661a366d2
Montclair Code Team <869c568+cbgpenguin@users.noreply.github.com>
2025-10-21 15:45:49 -0400

    Update Elevator.java

--- src/main/java/Subsystems/Elevator.java
+++ src/main/java/Subsystems/Elevator.java
@@ -1,8 +1,17 @@
 package Subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Elevator extends SubsystemBase{
-    public static void main(String[]args){
         int speed;
+        private final int LEFT_MOTOR_ID = 1;
+        private final int RIGHT_MOTOR_ID = 1;
+        public TalonFX rightTalonFX;
+        public TalonFX leftTalonFX;
+
+public Elevator (){
+    rightTalonFX = new TalonFX (RIGHT_MOTOR_ID, "Drivetrain");
+    leftTalonFX = new TalonFX (LEFT_MOTOR_ID, "Drivetrain");
     }
 }
