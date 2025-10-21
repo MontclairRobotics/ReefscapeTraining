01c730eef9f59e3595165c2f8c2480ab387399b8
Montclair Code Team <869c568+cbgpenguin@users.noreply.github.com>
2025-10-21 15:27:03 -0400

    Update Elevator.java

--- src/main/java/Subsystems/Elevator.java
+++ src/main/java/Subsystems/Elevator.java
@@ -1,7 +1,8 @@
-public class Elevator {
+package Subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
     public static void main(String[]args){
         int speed;
-        
-        // declare controllers, buttons, 
     }
 }
