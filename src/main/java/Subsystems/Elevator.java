150593bd05330b61868ba6065a563475b86de4c1
Montclair Code Team <869c568+cbgpenguin@users.noreply.github.com>
2025-10-23 14:41:28 -0400

    Update Elevator.java

--- src/main/java/Subsystems/Elevator.java
+++ src/main/java/Subsystems/Elevator.java
@@ -1,19 +1,39 @@
 package Subsystems;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Elevator extends SubsystemBase{
         public double speed;
-        public final double MAX_HEIGHT = 1;
-        public double timeToReachMaxHeight = MAX_HEIGHT/speed;
-        private final int LEFT_MOTOR_ID = 1;
-        private final int RIGHT_MOTOR_ID = 1;
+        public static final double STARTING_HEIGHT = 0.98;
+        public static final double MAX_EXTENSION = 1.22;
+        public static final double MAX_HEIGHT = STARTING_HEIGHT + MAX_EXTENSION;
+        private static final double ROTATIONS_PER_METER = 36 * 5 / 1000.0 * (1.0 / 9);
+        private static final double METER_PER_ROTATIONS = 1.0/ROTATIONS_PER_METER;
+        private static final double SLOW_DOWN_ZONE = 0;
+        private static final double SLOWEST_SPEED = 0;
+        public static final double MAX_VELO_RPS = 100.0;
+        public static final double MAX_ACCELERATION_RPS = 350.0;
+
+        private PIDController pidController;
+
+
+        private final int LEFT_MOTOR_ID = 0;
+        private final int RIGHT_MOTOR_ID = 0;
         public TalonFX rightTalonFX;
         public TalonFX leftTalonFX;
 
 public Elevator (){
     rightTalonFX = new TalonFX (RIGHT_MOTOR_ID, "Drivetrain");
     leftTalonFX = new TalonFX (LEFT_MOTOR_ID, "Drivetrain");
+    pidController = new PIDController(8.2697, 0, 0.068398);
     }
+
+public void goToHeight (double height){
+    double targetHeightFromRotations = height * ROTATIONS_PER_METER;
+    rightTalonFX.getPosition();
+
+}
 }
