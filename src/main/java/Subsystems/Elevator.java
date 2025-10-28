45e9cb5366957e2903e449d0f14e55fdca7a8d78
Montclair Code Team <869c568+cbgpenguin@users.noreply.github.com>
2025-10-28 15:38:03 -0400

    Update Elevator.java

--- src/main/java/Subsystems/Elevator.java
+++ src/main/java/Subsystems/Elevator.java
@@ -1,8 +1,10 @@
 package Subsystems;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Elevator extends SubsystemBase{
@@ -10,8 +12,8 @@ public class Elevator extends SubsystemBase{
         public static final double STARTING_HEIGHT = 0.98;
         public static final double MAX_EXTENSION = 1.22;
         public static final double MAX_HEIGHT = STARTING_HEIGHT + MAX_EXTENSION;
-        private static final double ROTATIONS_PER_METER = 36 * 5 / 1000.0 * (1.0 / 9);
-        private static final double METER_PER_ROTATIONS = 1.0/ROTATIONS_PER_METER;
+        private static final double METERS_PER_ROTATION = 36 * 5 / 1000.0 * (1.0 / 9);
+        private static final double ROTATIONS_PER_METER = 1.0/METERS_PER_ROTATION;
         private static final double SLOW_DOWN_ZONE = 0;
         private static final double SLOWEST_SPEED = 0;
         public static final double MAX_VELO_RPS = 100.0;
@@ -31,9 +33,16 @@ public Elevator (){
     pidController = new PIDController(8.2697, 0, 0.068398);
     }
 
-public void goToHeight (double height){
-    double targetHeightFromRotations = height * ROTATIONS_PER_METER;
-    rightTalonFX.getPosition();
+private double getCurrentHeight(){
+    double rightDisplacement = (rightTalonFX.getPosition().getValueAsDouble());
+    double leftDisplacement = (rightTalonFX.getPosition().getValueAsDouble());
+    double averageDisplacement = (rightDisplacement + leftDisplacement/2.0);
+    return averageDisplacement * METERS_PER_ROTATION;
 
+}
+public void goToHeight (double targetHeight){
+    double pidOutput = pidController.calculate(getCurrentHeight(), targetHeight);
+    rightTalonFX.setVoltage(MathUtil.applyDeadband(pidOutput, 24.0));
+    leftTalonFX.setVoltage(MathUtil.applyDeadband(pidOutput, 24.0));
 }
 }
