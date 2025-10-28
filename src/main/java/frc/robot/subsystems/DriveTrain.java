06373b289ff388aa3c91063589bf3fe66f1aea77
omniscient-cookie <vainsencher.or@gmail.com>
2025-10-28 17:16:01 -0400

    finished robot relative and added documentation
    Co-authored-by: WhyTho? <James1348@users.noreply.github.com>
    Co-authored-by: A-Vegetable <A-Vegetable@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -7,28 +8,34 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.PS5Controller;
+
+
+/**
+ * the drivetrain subsystem.
+ * 
+ * 
+ */
 public class DriveTrain implements Subsystem{
-    
+    // the subsystem wide variables
     SwerveModule frontLeft;
     SwerveModule frontRight;      
     SwerveModule backLeft;
     SwerveModule backRight;
-    ChassisSpeeds speeds = new ChassisSpeeds(3.0, -2.0, Math.PI); // not supposed to be hardcoded i think
-
+    PIDController drivePID;
+    PIDController rotPID;
+    
     SwerveDriveKinematics kine = new SwerveDriveKinematics(
         DTConstants.frontLeftLocation,
         DTConstants.frontRightLocation,
         DTConstants.backLeftLocation,
         DTConstants.backRightLocation
         );
-    SwerveModuleState[] moduleStates = kine.toSwerveModuleStates(speeds);
-    SwerveModuleState frontLeftState = moduleStates[0];
-    SwerveModuleState frontRightState = moduleStates[1];
-    SwerveModuleState backLefState = moduleStates[2];
-    SwerveModuleState backRightState = moduleStates[3];
+
     
                
     public DriveTrain(){
+        drivePID = new PIDController(DTConstants.drkP,DTConstants.drkI,DTConstants.drkD);
+        rotPID = new PIDController(DTConstants.rotkP,DTConstants.rotkI,DTConstants.rotkD);
         
         frontRight = new SwerveModule(-1,-1); //TODO: find the IDs???????
         frontLeft = new SwerveModule(-1,-1); //TODO: find the IDs???????
@@ -36,10 +43,58 @@ public class DriveTrain implements Subsystem{
         backLeft = new SwerveModule(-1,-1); //TODO: find the IDs???????
 
     }
-    public void driveOrSmthIGuessIdk(PS5Controller driveController){
+    public void driveRobotRelative(PS5Controller driveController){
+        //chassis speeds based on drive controller
         ChassisSpeeds speed = new ChassisSpeeds(
             Math.pow(-driveController.getLeftY(),3)*DTConstants.maxSpeed,
             Math.pow(driveController.getLeftX(),3)*DTConstants.maxSpeed,
-            Math.pow(-driveController.getRightX(),3)*DTConstants.maxRotation);       
+            Math.pow(-driveController.getRightX(),3)*DTConstants.maxRotation);
+        driveFromChassisSpeeds(speed);
+    }
+    /**
+     * good luck
+     * @param speed the chassis speeds object you get from another drive funtion
+     */
+    public void driveFromChassisSpeeds(ChassisSpeeds speed){
+
+        //the module states from the chassis speed
+        SwerveModuleState[] moduleStates = kine.toSwerveModuleStates(speed); //99% sure this is supposed to be within a function
+        SwerveModuleState frontLeftState = moduleStates[0];
+        SwerveModuleState frontRightState = moduleStates[1];
+        SwerveModuleState backLeftState = moduleStates[2];
+        SwerveModuleState backRightState = moduleStates[3];
+        
+        //I'm so sorry.
+        //Me Too.
+        frontLeft.setVoltage(
+            drivePID.calculate(
+                frontLeft.driveMotor.getBusVoltage(), //yes, the measurement and setpoint are different units.  
+                frontLeftState.speedMetersPerSecond),//we're pretty sure it works. we hope it works.
+            rotPID.calculate(
+                frontLeft.turnMotor.getAbsoluteEncoder().getPosition(),
+                frontLeftState.angle.getRadians()));
+        frontRight.setVoltage(
+            drivePID.calculate(
+                frontRight.driveMotor.getBusVoltage(),
+                frontRightState.speedMetersPerSecond),
+            rotPID.calculate(
+                frontRight.turnMotor.getAbsoluteEncoder().getPosition(),
+                frontRightState.angle.getRadians()));
+        backLeft.setVoltage(
+            drivePID.calculate(
+                backLeft.driveMotor.getBusVoltage(),
+                backLeftState.speedMetersPerSecond),
+            rotPID.calculate(
+                backLeft.turnMotor.getAbsoluteEncoder().getPosition(),
+                backLeftState.angle.getRadians()));
+        backRight.setVoltage(
+            drivePID.calculate(
+                backRight.driveMotor.getBusVoltage(),
+                backRightState.speedMetersPerSecond),
+            rotPID.calculate(
+                backRight.turnMotor.getAbsoluteEncoder().getPosition(),
+                backRightState.angle.getRadians()));   
     }
 }
+//lowk lock in
+//lowk throw away the key
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -9,12 +9,19 @@ import com.revrobotics.spark.SparkMax;
 public class SwerveModule {
     SparkMax driveMotor;
     SparkMax turnMotor;
-    
+    /**
+     * @param ID of the Drive Motor
+     * @param ID of the Turn Motor 
+     */
     public SwerveModule(int driveID, int turnID){
         driveMotor = new SparkMax(driveID, MotorType.kBrushless);
         turnMotor = new SparkMax(turnID, MotorType.kBrushless);
     }
-    public void useSwerveModuleStates(SwerveModuleState state){
-        
+    /**
+     * Sets The Voltage Of A Drive Motor and a Turn Motor 
+     */
+    public void setVoltage(double driveVoltage, double rotVoltage){
+        driveMotor.setVoltage(driveVoltage);
+        turnMotor.setVoltage(rotVoltage);
     }
 }
