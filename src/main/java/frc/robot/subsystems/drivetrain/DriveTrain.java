c582a64b6332dfaa69736b61f9e0b7bd5a69f745
omniscient-cookie <vainsencher.or@gmail.com>
2025-12-03 07:51:42 -0500

    Connor's requested changes

--- src/main/java/frc/robot/subsystems/drivetrain/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DriveTrain.java
@@ -1,8 +1,4 @@
 package frc.robot.subsystems.drivetrain;
-import java.util.concurrent.Flow.Publisher;
-
-import org.littletonrobotics.junction.AutoLog;
-import org.littletonrobotics.junction.AutoLogOutput;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
 
@@ -10,25 +6,22 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import frc.robot.RobotContainer;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.networktables.DoublePublisher;
-import edu.wpi.first.networktables.DoubleTopic;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.NetworkTableValue;
 
 // if it has title case, its james, if it has no caps it's or (yes that's his name) and dashiell is also here in case anyone forgot 
 /**
  * the drivetrain subsystem.
  * 
  */
-public class DriveTrain implements Subsystem{
+public class DriveTrain extends SubsystemBase {
     // the subsystem wide variables
     boolean fieldRelative = true;
+
     SwerveDriveKinematics driveKinematics = new SwerveDriveKinematics(
         DTConstants.frontLeftLocation,
         DTConstants.frontRightLocation,
@@ -41,7 +34,6 @@ public class DriveTrain implements Subsystem{
     NetworkTable frTable = table.getSubTable("fr");
     NetworkTable blTable = table.getSubTable("bl");
     NetworkTable brTable = table.getSubTable("br");
-    NetworkTable[] subTables = {flTable,frTable,blTable,brTable};
 
     Pigeon2 gyro = new Pigeon2(DTConstants.gyroID);
     SwerveModule[] swerveModules = new SwerveModule[] { //fl,fr,bl,br
@@ -51,15 +43,10 @@ public class DriveTrain implements Subsystem{
             new SwerveModule(DTConstants.backRightDriveID, DTConstants.backRightRotID, DTConstants.backRightEncoder, DTConstants.backRightOffset,brTable),
     };
     
-
-    
-
-
-    
     public void driveJoystick(CommandPS5Controller driveController, boolean fieldRelative){
-        double x = Math.pow(-MathUtil.applyDeadband(driveController.getLeftY(),0.001),3)*DTConstants.maxSpeed;
-        double y = Math.pow(MathUtil.applyDeadband(driveController.getLeftX(),0.001),3)*DTConstants.maxSpeed;
-        double rot = Math.pow(-MathUtil.applyDeadband(driveController.getRightX(),0.001),3)*DTConstants.maxRot;
+        double x = Math.pow(-MathUtil.applyDeadband(driveController.getLeftY(),0.08),3)*DTConstants.maxSpeed;
+        double y = Math.pow(MathUtil.applyDeadband(driveController.getLeftX(),0.08),3)*DTConstants.maxSpeed;
+        double rot = Math.pow(-MathUtil.applyDeadband(driveController.getRightX(),0.08),3)*DTConstants.maxRot;
         if(fieldRelative) {
             driveFieldRelative(x,y,rot);
         }
@@ -68,14 +55,14 @@ public class DriveTrain implements Subsystem{
         }
     }
 
-    public void driveRobotRelative(double x,double y,double rot){
+    public void driveRobotRelative(double x, double y, double rot){
         //chassis speeds based on drive controller
         ChassisSpeeds speed = new ChassisSpeeds(x,y,rot);
         driveFromChassisSpeeds(speed);
     }
 
-    public void driveFieldRelative(double x,double y,double rot){
-        ChassisSpeeds speed = ChassisSpeeds.fromFieldRelativeSpeeds(x,y,rot,gyro.getRotation2d());
+    public void driveFieldRelative(double x, double y, double rot){
+        ChassisSpeeds speed = ChassisSpeeds.fromFieldRelativeSpeeds(x, y, rot, gyro.getRotation2d());
         driveFromChassisSpeeds(speed);
     }
 
@@ -88,7 +75,7 @@ public class DriveTrain implements Subsystem{
         //the module states from the chassis speed
         SwerveModuleState[] moduleStates = driveKinematics.toSwerveModuleStates(speed);
         
-        for(int i = 0; i < 4;i++){
+        for(int i = 0; i < 4; i++){
             swerveModules[i].fromModuleState(moduleStates[i]);
         } 
     }
@@ -104,5 +91,4 @@ public class DriveTrain implements Subsystem{
     public Command fieldRelToggleCommand(){
         return Commands.runOnce(()->{fieldRelativeToggle();});
     }
-    
 }
--- src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
@@ -5,78 +5,89 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.DoubleTopic;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.spark.SparkMax;
 
-public class SwerveModule {
+
+public class SwerveModule extends SubsystemBase {
     TalonFX driveMotor;
     SparkMax rotMotor;
     PIDController drivePID;
     PIDController rotPID;
+
     double encoderOffset;
     CANcoder encoder;
+
     NetworkTable table;
+    NetworkTable angleTable;
+    NetworkTable speedTable;
+    DoubleTopic speedSetpointTop;
+    DoubleTopic speedMeasureTop;
+    DoubleTopic speedVoltageTop;
+    DoubleTopic angleSetpointTop;
+    DoubleTopic angleMeasureTop;
     
-        /**
-         * @param ID of the Drive Motor
-         * @param ID of the Rotation Motor 
-         */
-        public SwerveModule(int driveID, int rotID,int encoderID, double offset, NetworkTable netTable){
-            driveMotor = new TalonFX(driveID);
-            rotMotor = new SparkMax(rotID, MotorType.kBrushless);
-            drivePID = new PIDController(DTConstants.drkP,DTConstants.drkI,DTConstants.drkD);
-            rotPID = new PIDController(DTConstants.rotkP,DTConstants.rotkI,DTConstants.rotkD);
-            rotPID.enableContinuousInput(-Math.PI, Math.PI);
-            encoder = new CANcoder(encoderID);
-            encoderOffset = offset;
-            
-            table = netTable;
-                
-        }
-
-        /**
-         * Sets The Voltage Of A Drive Motor and a Rotation Motor 
-         */
+    /**
+     * @param ID of the Drive Motor
+     * @param ID of the Rotation Motor 
+     */
+    public SwerveModule(int driveID, int rotID,int encoderID, double offset, NetworkTable netTable){
+        driveMotor = new TalonFX(driveID);
+        rotMotor = new SparkMax(rotID, MotorType.kBrushless);
+        drivePID = new PIDController(DTConstants.drkP,DTConstants.drkI,DTConstants.drkD);
+        rotPID = new PIDController(DTConstants.rotkP,DTConstants.rotkI,DTConstants.rotkD);
+        rotPID.enableContinuousInput(-Math.PI, Math.PI);
+        encoder = new CANcoder(encoderID);
+        encoderOffset = offset;
         
-        public void logging(double speedMeasure,double speedSetpoint, double speedVoltage, double angleMeasure, double angleSetpoint){
-            NetworkTable angleTable = table.getSubTable("Angle");
-            NetworkTable speedTable = table.getSubTable("Speed");
+        table = netTable;
             
-            DoubleTopic speedSetpointTop =  speedTable.getDoubleTopic("setpoint");
-            DoubleTopic speedMeasureTop =  speedTable.getDoubleTopic("measure");
-            DoubleTopic speedVoltageTop = speedTable.getDoubleTopic("voltage");
-            
-            DoubleTopic angleSetpointTop =  angleTable.getDoubleTopic("setpoint");
-            DoubleTopic angleMeasureTop =  angleTable.getDoubleTopic("measure");
-            
+        angleTable = table.getSubTable("Angle");
+        speedTable = table.getSubTable("Speed");
+        
+        speedSetpointTop =  speedTable.getDoubleTopic("setpoint");
+        speedMeasureTop =  speedTable.getDoubleTopic("measure");
+        speedVoltageTop = speedTable.getDoubleTopic("voltage");
+        
+        angleSetpointTop =  angleTable.getDoubleTopic("setpoint");
+        angleMeasureTop =  angleTable.getDoubleTopic("measure");
+    }
 
-            speedSetpointTop.publish().set(speedSetpoint);
-            speedMeasureTop.publish().set(speedMeasure);
-            speedVoltageTop.publish().set(speedVoltage);
+    /**
+     * Sets The Voltage Of A Drive Motor and a Rotation Motor 
+     */
+    public void logging(double speedMeasure,double speedSetpoint, double speedVoltage, double angleMeasure, double angleSetpoint) {
 
-            angleSetpointTop.publish().set(angleSetpoint);
-            angleMeasureTop.publish().set(angleMeasure);
-        }
-        public void driveWithVoltage(double driveVoltage, double rotVoltage){
-            driveMotor.setVoltage(driveVoltage);
-            rotMotor.setVoltage(rotVoltage);
-        }
-        public void fromModuleState(SwerveModuleState moduleState) {
-            // encoder gives position as rotation, so we have to multiply by 2PI to get to radians.
-            double currentRot = encoder.getPosition().getValue().magnitude()*2*Math.PI+encoderOffset;
-            double currentVelocity = driveMotor.getVelocity().getValue().magnitude()*0.407; // velocity is in rotations per second, so we have to multiply by a conversertion
-            double currentVoltage = driveMotor.getMotorVoltage().getValue().abs(driveMotor.getMotorVoltage().getValue().unit());
-            moduleState.optimize(Rotation2d.fromRadians(currentRot));
-            logging(currentVelocity,moduleState.speedMetersPerSecond,currentVoltage,currentRot,moduleState.angle.getRadians());
-            driveWithVoltage(
-                    drivePID.calculate(currentVelocity, 
-                                        moduleState.speedMetersPerSecond),                    
-                    rotPID.calculate(currentRot, moduleState.angle.getRadians()));
+        speedSetpointTop.publish().set(speedSetpoint);
+        speedMeasureTop.publish().set(speedMeasure);
+        speedVoltageTop.publish().set(speedVoltage);
+
+        angleSetpointTop.publish().set(angleSetpoint);
+        angleMeasureTop.publish().set(angleMeasure);
+    }
+
+    public void driveWithVoltage(double driveVoltage, double rotVoltage){
+        driveMotor.setVoltage(driveVoltage);
+        rotMotor.setVoltage(rotVoltage);
+    }
+
+    public void fromModuleState(SwerveModuleState moduleState) {
+        // encoder gives position as rotation, so we have to multiply by 2PI to get to radians.
+        double currentRot = encoder.getPosition().getValue().magnitude()*2*Math.PI+encoderOffset;
+        double currentVelocity = driveMotor.getVelocity().getValue().magnitude()*0.407; // velocity is in rotations per second, so we have to multiply by a conversion
+        double currentVoltage = driveMotor.getMotorVoltage().getValueAsDouble();
+
+        moduleState.optimize(Rotation2d.fromRadians(currentRot));
+
+        logging(currentVelocity,moduleState.speedMetersPerSecond,currentVoltage,currentRot,moduleState.angle.getRadians());
+
+        driveWithVoltage(
+                drivePID.calculate(currentVelocity, moduleState.speedMetersPerSecond),                    
+                rotPID.calculate(currentRot, moduleState.angle.getRadians()));
     }
 }
